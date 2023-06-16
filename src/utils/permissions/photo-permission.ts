26413d463b2157eb4dbf6c6d8164c90fb7fe3c4e
yunchanpark <yongp99@nate.com>
2023-06-16 16:38:43 +0900

    add: 앨범 접근 권한 요청 추가

---
+++ src/utils/permissions/photo-permission.ts
@@ -0,0 +1,23 @@
+import { Platform } from 'react-native';
+import { requestAndroidPermissions, requestIOSPermissions } from './request-permissions';
+
+const photoPermissionCheckIOS = async () => {
+    const permissionsResult = await requestIOSPermissions<['photo']>(['photo']);
+    return permissionsResult.photo;
+};
+
+const androidPhotoPermissionCheck = async () => {
+    const isLegacyVersion = Number(Platform.Version) >= 33;
+    if (isLegacyVersion) {
+        const permissionsResult = await requestAndroidPermissions<['legacyPhoto']>(['legacyPhoto']);
+        return permissionsResult.legacyPhoto;
+    }
+
+    const permissionsResult = await requestAndroidPermissions<['photo']>(['photo']);
+    return permissionsResult.photo;
+};
+
+export const photoPermissionCheck = async () => {
+    const hasPermission = Platform.OS === 'ios' ? await photoPermissionCheckIOS() : await androidPhotoPermissionCheck();
+    return hasPermission;
+};
---
+++ src/utils/permissions/request-permissions.ts
@@ -0,0 +1,58 @@
+import { check, RESULTS, request, PERMISSIONS } from 'react-native-permissions';
+
+export const IOS_PERMISSION = {
+    camera: PERMISSIONS.IOS.CAMERA,
+    photo: PERMISSIONS.IOS.PHOTO_LIBRARY,
+} as const;
+
+export const ANDROID_PERMISSION = {
+    camera: PERMISSIONS.ANDROID.CAMERA,
+    photo: PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+    legacyPhoto: PERMISSIONS.ANDROID.READ_MEDIA_IMAGES,
+} as const;
+
+export type PermissionsPerIOS = keyof typeof IOS_PERMISSION;
+
+export type PermissionsPerAndroid = keyof typeof ANDROID_PERMISSION;
+
+type PermissionStatus<T extends string[]> = { [K in T[number]]?: boolean };
+
+export const requestIOSPermissions = async <T extends PermissionsPerIOS[]>(
+    permissionKeys: PermissionsPerIOS[],
+): Promise<PermissionStatus<T>> => {
+    const statuses = await Promise.all(
+        permissionKeys.map(async (key) => {
+            const permission = IOS_PERMISSION[key];
+            const checkResult = await check(permission);
+
+            if (checkResult === RESULTS.DENIED) {
+                const requestResult = await request(permission);
+                return { key, status: requestResult };
+            }
+
+            return { key, status: checkResult };
+        }),
+    );
+
+    return Object.fromEntries(statuses.map(({ key, status }) => [key, status === RESULTS.GRANTED])) as PermissionStatus<T>;
+};
+
+export const requestAndroidPermissions = async <T extends PermissionsPerAndroid[]>(
+    permissionKeys: PermissionsPerAndroid[],
+): Promise<PermissionStatus<T>> => {
+    const statuses = await Promise.all(
+        permissionKeys.map(async (key) => {
+            const permission = ANDROID_PERMISSION[key];
+            const checkResult = await check(permission);
+
+            if (checkResult === RESULTS.DENIED) {
+                const requestResult = await request(permission);
+                return { key, status: requestResult };
+            }
+
+            return { key, status: checkResult };
+        }),
+    );
+
+    return Object.fromEntries(statuses.map(({ key, status }) => [key, status === RESULTS.GRANTED])) as PermissionStatus<T>;
+};
