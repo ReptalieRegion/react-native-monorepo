e5d26987cacff94fdc793d069452a131a0329ddf
yunchanpark <yongp99@nate.com>
2023-07-27 02:59:20 +0900

    add: PostCard 추가

---
+++ src/components/share-post/list/organisms/PostCard.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PostHeader from '../molecules/PostHeader';
+import InteractivePost from '../molecules/InteractivePost';
+import PostContent from '../molecules/PostContent';
+import PostImageCarousel from '../molecules/PostImageCarousel';
+import { ISharePostsData } from '<SharePostAPI>';
+import { StyleSheet, View } from 'react-native';
+
+const PostCard = ({
+    userId,
+    isFollow,
+    postId,
+    profile,
+    images,
+    name,
+    commentCount,
+    likeCount,
+    content,
+    isLike,
+}: ISharePostsData) => {
+    return (
+        <View style={styles.container}>
+            <PostHeader userId={userId} isFollow={isFollow} profile={profile} name={name} />
+            <PostImageCarousel postId={postId} images={images} />
+            <InteractivePost postId={postId} isLike={isLike} images={images} />
+            <PostContent likeCount={likeCount} commentCount={commentCount} content={content} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        marginBottom: 40,
+    },
+});
+
+export default PostCard;
