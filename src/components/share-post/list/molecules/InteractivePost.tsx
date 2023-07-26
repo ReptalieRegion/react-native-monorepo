e630f3dd6b31f723692e53738b8302a54396ba00
yunchanpark <yongp99@nate.com>
2023-07-27 02:09:19 +0900

    add: InteractivePost 추가

---
+++ src/components/share-post/list/molecules/InteractivePost.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Like from '../atoms/Like';
+import CommentIcon from '../atoms/CommentIcon';
+import ImagesIndicators from '../atoms/ImagesIndicators';
+import { ISharePostsData } from '<SharePostAPI>';
+import { StyleSheet, View } from 'react-native';
+
+type InteractivePostProps = Pick<ISharePostsData, 'isLike' | 'postId' | 'images'>;
+
+const InteractivePost = ({ postId, isLike, images }: InteractivePostProps) => {
+    return (
+        <View style={[styles.container, styles.flexRow]}>
+            <View style={[styles.flexRow, styles.likeCommentContent]}>
+                <Like postId={postId} isLike={isLike} />
+                <CommentIcon postId={postId} />
+            </View>
+            <ImagesIndicators images={images} postId={postId} />
+            <View style={styles.empty} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 5,
+    },
+    flexRow: {
+        flexDirection: 'row',
+    },
+    likeCommentContent: {
+        marginLeft: -5,
+    },
+    empty: {
+        width: 80,
+    },
+});
+
+export default InteractivePost;
