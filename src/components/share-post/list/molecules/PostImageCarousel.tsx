5b1fd49c59805b01f0bee147d5cdaecd1a4aea58
yunchanpark <yongp99@nate.com>
2023-07-27 02:56:11 +0900

    add: PostImageCarousel 추가

---
+++ src/components/share-post/list/molecules/PostImageCarousel.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { shallow } from 'zustand/shallow';
+import sharePostListStore from '@/stores/share-post/list';
+import DoubleTabView from '@/components/common/element/view/DoubleTabView';
+import Haptic from '@/utils/webview-bridge/react-native/haptic/Haptic';
+import HeartAnimation from '../atoms/HeartAnimation';
+import ImageContent from '../atoms/ImageContent';
+import { ISharePostsData } from '<SharePostAPI>';
+
+type ImagesSliderProps = Pick<ISharePostsData, 'images' | 'postId'>;
+
+const PostImageCarousel = ({ postId, images }: ImagesSliderProps) => {
+    const { startLikeAnimation, setStartLikeAnimation } = sharePostListStore(
+        (state) => ({
+            setStartLikeAnimation: state.setStartLikeAnimation,
+            startLikeAnimation: state.postsOfInfo[postId]?.startLikeAnimation,
+        }),
+        shallow,
+    );
+
+    const handleDoubleTabHeartAnimation = () => {
+        setStartLikeAnimation(postId, true);
+
+        if (!startLikeAnimation) {
+            Haptic.trigger({
+                type: 'impactLight',
+                options: { enableVibrateFallback: true, ignoreAndroidSystemSettings: false },
+            });
+        }
+    };
+
+    return (
+        <DoubleTabView onDoubleTab={handleDoubleTabHeartAnimation} style={styles.container}>
+            <ImageContent images={images} postId={postId} />
+            {startLikeAnimation && <HeartAnimation postId={postId} />}
+        </DoubleTabView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'relative',
+    },
+});
+
+export default PostImageCarousel;
