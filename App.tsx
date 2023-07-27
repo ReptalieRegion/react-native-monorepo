0ed877fb473293a4822f08f2d93132d760dfe0c1
yunchanpark <yongp99@nate.com>
2023-07-27 16:06:16 +0900

    add: react-query 설치

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import UIPromptsContextComponent from '@/contexts/ui-prompts/UIPromptsContext';
-import 'react-native-gesture-handler';
 import RootRoutes from '@/routes/RootRoutes';
+import UIPromptsContextComponent from '@/contexts/ui-prompts/UIPromptsContext';
+import ReactQueryContextComponent from '@/contexts/react-query/ReactQueryContext';
+import 'react-native-gesture-handler';
 
 export default function App() {
     return (
-        <UIPromptsContextComponent>
-            <RootRoutes />
-        </UIPromptsContextComponent>
+        <ReactQueryContextComponent>
+            <UIPromptsContextComponent>
+                <RootRoutes />
+            </UIPromptsContextComponent>
+        </ReactQueryContextComponent>
     );
 }
