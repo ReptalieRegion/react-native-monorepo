c41e290479771a684a26a624d7c5f22a62dcf1c4
yunchanpark <yongp99@nate.com>
2023-07-27 20:33:55 +0900

    shell: set-env 스크립트 수정

--- shell/set-env.sh
+++ shell/set-env.sh
@@ -2,5 +2,5 @@ cat >./src/const/config.ts <<EOF
 import { ModeType } from '@env';
 
 export const REACT_NATIVE_ENV: ModeType = '$1';
-export const CURRENT_IP = '';
+export const CURRENT_IP = '$(ipconfig getifaddr en0)';
 EOF
