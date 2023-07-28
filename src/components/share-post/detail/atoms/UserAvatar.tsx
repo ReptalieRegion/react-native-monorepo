bc4db93451fd93575be5069b273c06fc385c20be
yunchanpark <yongp99@nate.com>
2023-07-28 14:51:55 +0900

    add: UserAvatar 추가

---
+++ src/components/share-post/detail/atoms/UserAvatar.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { SharePostDetailPostsData } from '<SharePostAPI>';
+
+type UserAvatarProps = Pick<SharePostDetailPostsData, 'profile' | 'nickname'>;
+
+const UserAvatar = ({ profile, nickname }: UserAvatarProps) => {
+    return (
+        <View>
+            <View style={styles.imageContainer}>
+                <Image
+                    style={styles.image}
+                    source={{ uri: profile.src, width: 100, height: 100 }}
+                    alt={profile.alt}
+                    resizeMode="cover"
+                />
+            </View>
+            <Text style={styles.nickname}>{nickname}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    imageContainer: {
+        position: 'relative',
+    },
+    image: {
+        borderRadius: 9999,
+        width: 80,
+        height: 80,
+    },
+    nickname: {
+        fontWeight: '500',
+    },
+});
+
+export default UserAvatar;
