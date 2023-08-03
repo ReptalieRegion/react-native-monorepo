3bdb40d4575c5c3837436154fde325bd9bfb6db7
yunchanpark <yongp99@nate.com>
2023-08-03 12:55:23 +0900

    fix: kebabMenuBottomSheet 파일 위치 변경

--- src/components/share-post/list/atoms/PostKebabMenu.tsx
+++ src/components/share-post/list/atoms/PostKebabMenu.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { TouchableWithoutFeedback, View } from 'react-native';
 import KebabMenu from '@/assets/icons/KebabMenu';
 import { UIPromptsContext } from '@/contexts/ui-prompts/UIPromptsContext';
-import KebabMenuBottomSheet from '../ui-prompts/bottomSheet/KebabMenuBottomSheet';
+import KebabMenuBottomSheet from '../ui-prompts/bottomSheet/kebab-menu/KebabMenuBottomSheet';
 import { SharePostsData } from '<SharePostAPI>';
 
 type PostKebabProps = Pick<SharePostsData, 'postId' | 'userId'>;
