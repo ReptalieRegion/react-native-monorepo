fa51b4debb88458a6fd851352448626ec4a6aa36
yunchanpark <yongp99@nate.com>
2023-08-03 12:11:54 +0900

    add: react-native-reanimated todo 주석 추가

--- App.tsx
+++ App.tsx
@@ -5,8 +5,9 @@ import RootRoutes from '@/routes/RootRoutes';
 import UIPromptsContextComponent from '@/contexts/ui-prompts/UIPromptsContext';
 import ReactQueryContextComponent from '@/contexts/react-query/ReactQueryContext';
 import { StyleSheet } from 'react-native';
-import 'react-native-reanimated';
 import 'react-native-gesture-handler';
+/** @todo react-native-reanimated v3.4.2로 올라가면 이슈 확인 후 삭제 */
+import 'react-native-reanimated';
 
 export default function App() {
     return (
