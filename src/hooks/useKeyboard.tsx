bd19cfec5df98634a06d6ec6ed30886751a4ec7b
yunchanpark <yongp99@nate.com>
2023-08-04 20:57:16 +0900

    fix: useKeyboard 키보드 올라오기 직전인 상태도 추가

--- src/hooks/useKeyboard.tsx
+++ src/hooks/useKeyboard.tsx
@@ -1,26 +1,37 @@
 import { useEffect, useState } from 'react';
-import { Keyboard } from 'react-native';
+import { Keyboard, KeyboardEvent } from 'react-native';
 
 const useKeyboard = () => {
+    const [isKeyboardWillShow, setIsKeyboardWillShow] = useState<boolean>(false);
     const [isKeyboardShow, setIsKeyboardShow] = useState<boolean>(false);
+    const [keyboardHeight, setKeyboardHeight] = useState<number>(0);
+
     useEffect(() => {
-        const openKeyboard = () => {
+        const openKeyboard = (event: KeyboardEvent) => {
+            setKeyboardHeight(event.endCoordinates.height);
             setIsKeyboardShow(true);
         };
 
         const closeKeyboard = () => {
+            setKeyboardHeight(0);
             setIsKeyboardShow(false);
         };
 
+        const willOpenKeyboard = () => {
+            setIsKeyboardWillShow(true);
+        };
+
+        const keyboardWillShow = Keyboard.addListener('keyboardWillShow', willOpenKeyboard);
         const keyboardDidShow = Keyboard.addListener('keyboardDidShow', openKeyboard);
         const keyboardDidHide = Keyboard.addListener('keyboardDidHide', closeKeyboard);
 
         return () => {
+            keyboardWillShow.remove();
             keyboardDidShow.remove();
-            keyboardDidHide.remove;
+            keyboardDidHide.remove();
         };
     }, []);
-    return { isKeyboardShow };
+    return { isKeyboardShow, isKeyboardWillShow, keyboardHeight };
 };
 
 export default useKeyboard;
