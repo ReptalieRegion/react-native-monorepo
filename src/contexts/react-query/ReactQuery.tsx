59fb8cf20fe76649aa9fe589f08c922fb67eb549
yunchanpark <yongp99@nate.com>
2023-08-06 21:22:11 +0900

    add: react query devtools 적용

--- src/contexts/react-query/ReactQuery.tsx
+++ src/contexts/react-query/ReactQuery.tsx
@@ -3,6 +3,12 @@ import React, { PropsWithChildren } from 'react';
 
 const queryClient = new QueryClient();
 
+if (__DEV__) {
+    import('react-query-native-devtools').then(({ addPlugin }) => {
+        addPlugin({ queryClient });
+    });
+}
+
 const ReactQueryContextComponent = ({ children }: PropsWithChildren) => {
     return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 };
