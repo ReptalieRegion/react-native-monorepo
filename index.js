b7c921f8164ecfda89a7127582dfec6a7302b5ea
yunchanpark <yongp99@nate.com>
2023-08-06 21:12:57 +0900

    fix: msw DEV일때만 실행으로 변경

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-if (process.env.NODE_ENV === 'local') {
-    require('react-native-url-polyfill/auto');
-    const { native } = require('./src/mocks/native');
-
-    native.listen();
+if (__DEV__) {
+    import('react-native-url-polyfill/auto').then(() => {
+        import('./src/mocks/native').then(({ native }) => {
+            native.listen();
+        });
+    });
 }
 
 AppRegistry.registerComponent(appName, () => App);
