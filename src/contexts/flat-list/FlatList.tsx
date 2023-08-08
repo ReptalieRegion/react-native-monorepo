3428f7c8e1f705c56f0761f176749739519d9389
yunchanpark <yongp99@nate.com>
2023-08-08 22:15:46 +0900

    fix: flatList context 스크롤 마다 renderItem 리렌더링되던 현상 memo와 useCallback을 이용하여 해결

--- src/contexts/flat-list/FlatList.tsx
+++ src/contexts/flat-list/FlatList.tsx
@@ -1,22 +1,7 @@
 import { debounce } from 'lodash-es';
-import React, { ReactNode, createContext, useRef, useState, useCallback } from 'react';
+import React, { ReactNode, createContext, useRef, useState, useCallback, useMemo } from 'react';
 import { FlatListProps, NativeScrollEvent, NativeScrollPoint, NativeSyntheticEvent, StyleSheet, View } from 'react-native';
 import { FlatList, NativeViewGestureHandlerProps } from 'react-native-gesture-handler';
-
-interface FlatListContextComponentProps<ItemT>
-    extends React.PropsWithChildren<
-        FlatListProps<ItemT> & React.RefAttributes<FlatList<ItemT>> & NativeViewGestureHandlerProps
-    > {
-    fixedChildren?: {
-        renderItem: ReactNode;
-        position: {
-            bottom?: number | string | undefined;
-            top?: number | string | undefined;
-            left?: number | string | undefined;
-            right?: number | string | undefined;
-        };
-    };
-}
 
 type HorizontalDirectionState = 'left' | 'right' | 'none';
 type VerticalDirectionState = 'top' | 'bottom' | 'none';
@@ -41,13 +26,53 @@ const defaultValue: FlatListContextValue = {
 
 export const FlatListContext = createContext<FlatListContextValue>(defaultValue);
 
-export const FlatListContextComponent = <ItemT = any,>({
+type GestureFlatList<ItemT> = React.PropsWithChildren<
+    FlatListProps<ItemT> & React.RefAttributes<FlatList<ItemT>> & NativeViewGestureHandlerProps
+>;
+
+type FixedChildren = {
+    renderItem: ReactNode;
+    position: {
+        bottom?: number | string | undefined;
+        top?: number | string | undefined;
+        left?: number | string | undefined;
+        right?: number | string | undefined;
+    };
+};
+
+interface FlatListContextComponentProps<ItemT> extends GestureFlatList<ItemT> {
+    fixedChildren?: FixedChildren;
+}
+
+const FlatListComponent = <ItemT extends any>(props: GestureFlatList<ItemT>, ref: React.Ref<FlatList<ItemT>> | undefined) => {
+    return <FlatList ref={ref} {...props} />;
+};
+
+const MemoizedFlatList = <ItemT extends any>() =>
+    React.memo(React.forwardRef<FlatList<ItemT>, FlatListProps<ItemT>>(FlatListComponent));
+
+const FixedChildrenComponent = ({ fixedChildren }: { fixedChildren?: FixedChildren }) => {
+    if (fixedChildren === undefined) {
+        return null;
+    }
+
+    return <View style={[styles.fixedContainer, { ...fixedChildren.position }]}>{fixedChildren.renderItem}</View>;
+};
+
+export const FlatListContextComponent = <ItemT extends any>({
     onScroll,
     onScrollBeginDrag,
     onScrollEndDrag,
+    renderItem,
+    keyExtractor,
+    data,
+    contentContainerStyle,
+    refreshControl,
     scrollEventThrottle,
     fixedChildren,
-    ...flatRestProps
+    initialNumToRender,
+    maxToRenderPerBatch,
+    horizontal,
 }: FlatListContextComponentProps<ItemT>) => {
     const flatListRef = useRef<FlatList<ItemT>>(null);
     const contentOffsetRef = useRef<NativeScrollPoint>();
@@ -55,6 +80,7 @@ export const FlatListContextComponent = <ItemT = any,>({
     const [verticalDirection, setVerticalDirection] = useState<VerticalDirectionState>('none');
     const [overScrollingState, setOverScrollingState] = useState<OverScrollingState>('none');
     const [isScrolling, setIsScrolling] = useState<boolean>(false);
+    const MemoizedFlatListComponent = useMemo(() => MemoizedFlatList<ItemT>(), []);
 
     const scrollIntoView: ScrollIntoView = ({ offset, animated = true }) => {
         flatListRef.current?.scrollToOffset({
@@ -65,7 +91,7 @@ export const FlatListContextComponent = <ItemT = any,>({
 
     const calcScrollDirection = useCallback(
         (currentContentOffset: NativeScrollPoint) => {
-            if (flatRestProps.horizontal) {
+            if (horizontal) {
                 const prevX = contentOffsetRef.current?.x ?? 0;
                 const currentX = currentContentOffset.x;
                 const isLefScroll = currentX - prevX < 0;
@@ -79,7 +105,7 @@ export const FlatListContextComponent = <ItemT = any,>({
                 setVerticalDirection(isUpScroll ? 'top' : isDownScroll ? 'bottom' : 'none');
             }
         },
-        [flatRestProps.horizontal],
+        [horizontal],
     );
 
     const calcOverScrollingDirection = useCallback((event: NativeSyntheticEvent<NativeScrollEvent>) => {
@@ -92,47 +118,67 @@ export const FlatListContextComponent = <ItemT = any,>({
         setOverScrollingState(isOverScrollingTop ? 'top' : isOverScrollingBottom ? 'bottom' : 'none');
     }, []);
 
-    const scrollStart = () => {
+    const scrollStart = useCallback(() => {
         setIsScrolling(true);
-    };
+    }, []);
 
-    const scrollEnd = debounce(() => {
-        setIsScrolling(false);
-    }, 500);
+    const debouncedScrollEnd = useMemo(
+        () =>
+            debounce(() => {
+                setIsScrolling(false);
+            }, 500),
+        [], // dependencies 배열
+    );
 
-    const handleScrollBeginDrag = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-        scrollStart();
-        onScrollBeginDrag?.(event);
-    };
+    const scrollEnd = useCallback(debouncedScrollEnd, [debouncedScrollEnd]);
 
-    const handleScrollEndDrag = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-        scrollEnd();
-        onScrollEndDrag?.(event);
-    };
+    const handleScrollBeginDrag = useCallback(
+        (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+            scrollStart();
+            onScrollBeginDrag?.(event);
+        },
+        [onScrollBeginDrag, scrollStart],
+    );
 
-    const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-        const contentOffset = event.nativeEvent.contentOffset;
-        calcScrollDirection(contentOffset);
-        calcOverScrollingDirection(event);
-        contentOffsetRef.current = contentOffset;
-        onScroll?.(event);
-    };
+    const handleScrollEndDrag = useCallback(
+        (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+            scrollEnd();
+            onScrollEndDrag?.(event);
+        },
+        [onScrollEndDrag, scrollEnd],
+    );
+
+    const handleScroll = useCallback(
+        (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+            const contentOffset = event.nativeEvent.contentOffset;
+            calcScrollDirection(contentOffset);
+            calcOverScrollingDirection(event);
+            contentOffsetRef.current = contentOffset;
+            onScroll?.(event);
+        },
+        [calcOverScrollingDirection, calcScrollDirection, onScroll],
+    );
 
     return (
         <FlatListContext.Provider
-            value={{ horizontalDirection, verticalDirection, isScrolling, overScrollingState, scrollIntoView }}
+            value={{ horizontalDirection, isScrolling, overScrollingState, scrollIntoView, verticalDirection }}
         >
-            <FlatList
+            <MemoizedFlatListComponent
                 ref={flatListRef}
-                {...flatRestProps}
+                horizontal={horizontal}
+                contentContainerStyle={contentContainerStyle}
+                data={data}
+                keyExtractor={keyExtractor}
+                renderItem={renderItem}
+                refreshControl={refreshControl}
+                initialNumToRender={initialNumToRender}
+                maxToRenderPerBatch={maxToRenderPerBatch}
                 scrollEventThrottle={scrollEventThrottle ?? 16}
                 onScroll={handleScroll}
                 onScrollBeginDrag={handleScrollBeginDrag}
                 onScrollEndDrag={handleScrollEndDrag}
             />
-            {fixedChildren !== undefined && (
-                <View style={[styles.fixedContainer, { ...fixedChildren.position }]}>{fixedChildren.renderItem}</View>
-            )}
+            <FixedChildrenComponent fixedChildren={fixedChildren} />
         </FlatListContext.Provider>
     );
 };
