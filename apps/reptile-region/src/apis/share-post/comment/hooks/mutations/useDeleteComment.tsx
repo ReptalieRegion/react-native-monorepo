d136de3e4c9cfd613da2c1addd7c53ab3a855acd
yunchanpark <yongp99@nate.com>
2023-09-15 22:19:46 +0900

    fix: 댓글 삭제 캐시 업데이트 로직 추가

--- apps/reptile-region/src/apis/share-post/comment/hooks/mutations/useDeleteComment.tsx
+++ apps/reptile-region/src/apis/share-post/comment/hooks/mutations/useDeleteComment.tsx
@@ -2,9 +2,49 @@ import { InfiniteData, QueryClient, useMutation, useQueryClient } from '@tanstac
 
 import { deleteComment } from '../../repository';
 
+import { SharePostListInfiniteData } from '<SharePostAPI>';
 import type { DeleteCommentRequest, DeleteCommentResponse, SharePostCommentInfiniteData } from '<SharePostCommentAPI>';
 import { sharePostQueryKeys } from '@/apis/query-keys';
 
+// Cache Update: 일상공유 무한스크롤 조회 리스트
+const updateSharePostListCache = ({
+    queryClient,
+    post,
+}: {
+    queryClient: QueryClient;
+    post: Pick<DeleteCommentResponse['post'], 'id'>;
+}) => {
+    queryClient.setQueryData<InfiniteData<SharePostListInfiniteData>>(sharePostQueryKeys.list, (prevSharePostListData) => {
+        if (prevSharePostListData === undefined) {
+            return undefined;
+        }
+
+        const updatePages = [...prevSharePostListData.pages].map((page) => {
+            const items = page.items.map((item) => {
+                if (item.post.id === post.id) {
+                    return {
+                        ...item,
+                        post: {
+                            ...item.post,
+                            commentCount: item.post.commentCount - 1,
+                        },
+                    };
+                }
+
+                return item;
+            });
+
+            return { ...page, items };
+        });
+
+        return {
+            ...prevSharePostListData,
+            pages: updatePages,
+        };
+    });
+};
+
+// Cache Update: 특정 게시글 댓글 리스트 무한 스크롤
 const deleteCommentCache = ({ queryClient, data }: { queryClient: QueryClient; data: DeleteCommentResponse }) => {
     queryClient.setQueryData<InfiniteData<SharePostCommentInfiniteData>>(
         sharePostQueryKeys.comment(data.post.id),
@@ -33,6 +73,7 @@ const useDeleteComment = () => {
         mutationFn: ({ commentId }) => deleteComment({ commentId }),
         onSuccess: (data) => {
             deleteCommentCache({ queryClient, data });
+            updateSharePostListCache({ queryClient, post: data.post });
         },
     });
 };
