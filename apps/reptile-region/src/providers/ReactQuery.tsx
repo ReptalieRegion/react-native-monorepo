e1bbd4e8ac72c2beed38a97bbff8923cbd376cbc
yunchanpark <yongp99@nate.com>
2023-09-21 03:46:09 +0900

    fix: ReactQuery 코드 위치 수정

--- apps/reptile-region/src/providers/ReactQuery.tsx
+++ apps/reptile-region/src/providers/ReactQuery.tsx
@@ -1,6 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { PropsWithChildren } from 'react';
 
+if (__DEV__) {
+    import('react-query-native-devtools').then(({ addPlugin }) => {
+        addPlugin({ queryClient });
+    });
+}
+
 const queryClient = new QueryClient({
     defaultOptions: {
         mutations: {
@@ -10,12 +16,6 @@ const queryClient = new QueryClient({
         },
     },
 });
-
-if (__DEV__) {
-    import('react-query-native-devtools').then(({ addPlugin }) => {
-        addPlugin({ queryClient });
-    });
-}
 
 const ReactQueryProvider = ({ children }: PropsWithChildren) => {
     return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
