5d89dc001088b4f29913d27b6063ca6847725976
yunchanpark <yongp99@nate.com>
2023-10-09 17:01:09 +0900

    fix: useInfiniteComment staleTime 수정

--- apps/reptile-region/src/apis/share-post/comment/hooks/queries/useInfiniteComment.tsx
+++ apps/reptile-region/src/apis/share-post/comment/hooks/queries/useInfiniteComment.tsx
@@ -10,7 +10,7 @@ const useInfiniteComment = ({ postId }: FetchComment['Request']) => {
         queryKey: sharePostQueryKeys.comment(postId),
         queryFn: ({ pageParam }) => getComments({ pageParam, postId }),
         getNextPageParam: (lastPage) => lastPage.nextPage,
-        staleTime: 5 * 60 * 1000,
+        staleTime: 1 * 60 * 1000,
         cacheTime: 5 * 60 * 1000,
         suspense: true,
     });
