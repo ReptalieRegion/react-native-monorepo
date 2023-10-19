fb200b18538bff0a40ab5aa3e86855b5d50443f0
yunchanpark <yongp99@nate.com>
2023-10-19 18:10:43 +0900

    fix: 일상공유 수정 최소 이미지 1개 limit 로직 추가

--- apps/reptile-region/src/components/share-post/organisms/PostUpdate/reducer/image-reducer.ts
+++ apps/reptile-region/src/components/share-post/organisms/PostUpdate/reducer/image-reducer.ts
@@ -1,11 +1,22 @@
 import type { PostUpdateImageActions, PostUpdateImageState } from '../types';
 
+const deleteImage = (state: PostUpdateImageState, targetUri: string): PostUpdateImageState => {
+    console.log(state);
+    if (state.images.length === 1) {
+        return { ...state, state: 'MIN_IMAGE' };
+    }
+
+    return { ...state, images: state.images.filter((image) => image.src !== targetUri), state: '' };
+};
+
 const imageReducer = (state: PostUpdateImageState, actions: PostUpdateImageActions): PostUpdateImageState => {
     switch (actions.type) {
         case 'DELETE_IMAGE':
-            return { ...state, images: state.images.filter((image) => image.src !== actions.uri) };
+            return deleteImage(state, actions.uri);
         case 'INIT_IMAGE':
             return { ...state, images: actions.images };
+        case 'RESET_STATE':
+            return { ...state, state: '' };
         default:
             return state;
     }
