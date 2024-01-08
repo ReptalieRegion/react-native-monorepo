48c8aaaca40b57e7c776f51aad92ff09f59ad108
yunchanpark <yongp99@nate.com>
2024-01-09 00:29:09 +0900

    fix: ios status bar prod 환경일 때 색 변경

--- apps/reptile-region/src/components/@common/atoms/StatusBar/index.tsx
+++ apps/reptile-region/src/components/@common/atoms/StatusBar/index.tsx
@@ -5,7 +5,15 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import ENV from '@/env';
 
-const backgroundColor = ENV.isProd ? undefined : ENV.isDev ? 'red' : ENV.isLocal ? undefined : color.DarkGray[500].toString();
+const backgroundColor = ENV.isProd
+    ? Platform.OS === 'ios'
+        ? color.White.toString()
+        : undefined
+    : ENV.isDev
+      ? 'red'
+      : ENV.isLocal
+        ? undefined
+        : color.DarkGray[500].toString();
 
 export default function MainStatusBar() {
     const { top } = useSafeAreaInsets();
