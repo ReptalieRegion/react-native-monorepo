8c5b62f5f602d3eb03419652c6c36704256560c0
yunchanpark <yongp99@nate.com>
2024-01-12 22:05:40 +0900

    delete: 바텀시트 사용안하는 컴포넌트 삭제

--- packages/bottom-sheet/src/components/index.ts
+++ packages/bottom-sheet/src/components/index.ts
@@ -1,3 +1,2 @@
 export { default as BottomSheet } from './BottomSheet';
 export { default as BottomSheetAnimatedGesture } from './BottomSheetAnimatedGesture';
-export { default as BottomSheetPadding } from './BottomSheetPadding';
