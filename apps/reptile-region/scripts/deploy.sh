6bb4d8b5b30fef11b4d31323b80675553a42a662
yunchanpark <yongp99@nate.com>
2024-01-26 12:34:43 +0900

    fix: 배포 스크립트 변경

--- apps/reptile-region/scripts/deploy.sh
+++ apps/reptile-region/scripts/deploy.sh
@@ -2,6 +2,7 @@
 VERSION=$*
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+sh ./scripts/set-env.sh production
 node ./scripts/extract-licenses/index.js
 cd ${SCRIPT_DIR}/../ios/fastlane
 fastlane beta version:${VERSION}
