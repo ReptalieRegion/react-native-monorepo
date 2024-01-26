3f8e1ce13d4432f9c7209f48276fcf8a6e4b9822
yunchanpark <yongp99@nate.com>
2024-01-26 15:55:27 +0900

    add: 패키지 테스트할 playground app 추가

---
+++ apps/playground/metro.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const fs = require('fs');
+const escape = require('escape-string-regexp');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
+
+const root = path.resolve(__dirname, '../..');
+const packages = path.resolve(root, 'packages');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const workspaces = fs
+    .readdirSync(packages)
+    .map((p) => path.join(packages, p))
+    .filter((p) => fs.statSync(p).isDirectory() && fs.existsSync(path.join(p, 'package.json')));
+
+const modules = workspaces
+    .flatMap((it) => {
+        const pak = JSON.parse(fs.readFileSync(path.join(it, 'package.json'), 'utf8'));
+
+        return pak.peerDependencies ? Object.keys(pak.peerDependencies) : [];
+    })
+    .sort()
+    .filter((m, i, self) => {
+        return self.lastIndexOf(m) === i && !m.startsWith('@crawl/');
+    });
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+    projectRoot: __dirname,
+    watchFolders: [root],
+
+    resolver: {
+        blacklistRE: exclusionList(
+            [].concat(
+                ...workspaces.map((it) => modules.map((m) => new RegExp(`^${escape(path.join(it, 'node_modules', m))}\\/.*$`))),
+            ),
+        ),
+
+        extraNodeModules: modules.reduce((acc, name) => {
+            acc[name] = path.join(root, 'node_modules', name);
+            return acc;
+        }, {}),
+    },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
