ec449d0b873e218265b6f005aabc819d7ac06623
yunchanpark <yongp99@nate.com>
2024-01-29 02:14:31 +0900

    fix: iOS dev, prod 환경 분리 배포

--- apps/crawl/scripts/ios-simulator.sh
+++ apps/crawl/scripts/ios-simulator.sh
@@ -5,7 +5,7 @@ VERSION=$3
 
 if [[ -z "$VERSION" ]]
 then
-    yarn react-native run-ios --scheme $SCHEME --mode $MODE --simulator iPhone SE
+    yarn react-native run-ios --scheme $SCHEME --simulator iPhone SE
 else
-    yarn react-native run-ios --scheme $SCHEME --mode $MODE --simulator "iPhone $VERSION"
+    yarn react-native run-ios --scheme $SCHEME --simulator iPhone $VERSION
 fi
