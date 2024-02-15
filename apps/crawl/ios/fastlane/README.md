ae4d9261bcc28bc84ba29869ad1db2d4276ef23a
yunchanpark <yongp99@nate.com>
2024-02-15 16:29:55 +0900

    fix: 테스트 배포 및 fastlane ios 자동 버전업 수정

--- apps/crawl/ios/fastlane/README.md
+++ apps/crawl/ios/fastlane/README.md
@@ -15,14 +15,6 @@ For _fastlane_ installation instructions, see [Installing _fastlane_](https://do
 
 ## iOS
 
-### ios versionup
-
-```sh
-[bundle exec] fastlane ios versionup
-```
-
-iOS version up
-
 ### ios release
 
 ```sh
--- apps/crawl/scripts/deploy.sh
+++ apps/crawl/scripts/deploy.sh
@@ -3,7 +3,14 @@ MODE=$1
 VERSION=$2
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sh ./scripts/set-env.sh production
 node ./scripts/extract-licenses/index.js
 cd ${SCRIPT_DIR}/../ios/fastlane
-fastlane ${MODE} version:${VERSION}
+
+if [[ -z "$VERSION" ]]
+then
+    fastlane ${MODE}
+else
+    fastlane ${MODE} version:${VERSION}
+fi
+
+
