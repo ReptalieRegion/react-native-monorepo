8ab852b5bbe6192078c331b88bedfe31a1450ec5
yunchanpark <yongp99@nate.com>
2024-02-27 21:35:47 +0900

    fix: 분양 상세 페이지 컴포넌트 분리

--- apps/crawl/src/pages/adoption/Detail/header.tsx
+++ apps/crawl/src/pages/adoption/Detail/header.tsx
@@ -1,28 +1,40 @@
-import type { NativeStackHeaderProps } from '@react-navigation/native-stack';
+import { color } from '@crawl/design-system';
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-import { NotificationIcon } from '@/assets/icons';
-import { createNativeStackHeader } from '@/components/@common/molecules';
-import useAuthNavigation from '@/hooks/auth/useNavigationAuth';
+import { BackButton, KebabMenu } from '@/assets/icons';
+import { HEADER_HEIGHT } from '@/constants/global';
+import type { AdoptionDetailPageScreen } from '@/types/routes/props/adoption/list';
 
-export function AdoptionDetailHeader(props: NativeStackHeaderProps) {
-    const { requireAuthNavigation } = useAuthNavigation();
-    const handlePressNotification = () => {
-        requireAuthNavigation(() => {
-            props.navigation.navigate('me/notification-log');
-        });
+export function AdoptionDetailHeader({ navigation }: Pick<AdoptionDetailPageScreen, 'navigation'>) {
+    const { top } = useSafeAreaInsets();
+    const navigateBack = () => {
+        navigation.goBack();
     };
 
-    return createNativeStackHeader({
-        leftIcon: 'logo',
-        right: (
-            <TouchableOpacity onPress={handlePressNotification}>
-                <NotificationIcon />
+    return (
+        <View style={[styles.container, { paddingTop: top }]}>
+            <TouchableOpacity onPress={navigateBack}>
+                <BackButton fill={color.White.toString()} width={30} height={30} />
             </TouchableOpacity>
-        ),
-        containerStyle: {
-            opacity: 0,
-        },
-    })(props);
+            <KebabMenu fill={color.White.toString()} width={30} height={30} />
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        flexDirection: 'row',
+        width: '100%',
+        paddingHorizontal: 20,
+        height: HEADER_HEIGHT,
+        justifyContent: 'space-between',
+        zIndex: 20,
+    },
+});
--- apps/crawl/src/pages/adoption/Detail/page.tsx
+++ apps/crawl/src/pages/adoption/Detail/page.tsx
@@ -11,17 +11,14 @@ import {
 } from 'react-native';
 import Animated, { interpolate, useAnimatedRef, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 import type { AnimatedScrollView } from 'react-native-reanimated/lib/typescript/reanimated2/component/ScrollView';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+import AdoptionDetailContent from './components/AdoptionDetailContent';
+import { AdoptionDetailHeader } from './header';
 
 import useSuspenseFetchAdoptionPost from '@/apis/adoption/hooks/queries/useSuspenseFetchAdoptionPost';
-import { BackButton, KebabMenu } from '@/assets/icons';
-import { Avatar } from '@/components/@common/atoms';
 import LikeIcon from '@/components/Like';
 import PageWrapper from '@/components/PageWrapper';
-import { HEADER_HEIGHT } from '@/constants/global';
 import type { AdoptionDetailPageScreen } from '@/types/routes/props/adoption/list';
-import { calculateTimeAgo } from '@/utils/date';
-import { formatToGender } from '@/utils/format/formatToGender';
 import { formatToKRW } from '@/utils/format/formatToKRW';
 
 const AnimatedImage = Animated.createAnimatedComponent(Image);
@@ -35,7 +32,6 @@ export default function AdoptionDetailPage({
     },
 }: AdoptionDetailPageScreen) {
     const scrollViewRef = useAnimatedRef<AnimatedScrollView>();
-    const { top } = useSafeAreaInsets();
     const { data } = useSuspenseFetchAdoptionPost({ adoptionId });
     const scrollY = useSharedValue(0);
 
@@ -51,18 +47,9 @@ export default function AdoptionDetailPage({
         };
     });
 
-    const navigateBack = () => {
-        navigation.goBack();
-    };
-
     return (
         <PageWrapper style={styles.wrapper}>
-            <View style={[styles.headerWrapper, { paddingTop: top }]}>
-                <TouchableOpacity onPress={navigateBack}>
-                    <BackButton fill={color.White.toString()} width={30} height={30} />
-                </TouchableOpacity>
-                <KebabMenu fill={color.White.toString()} width={30} height={30} />
-            </View>
+            <AdoptionDetailHeader navigation={navigation} />
             <Animated.ScrollView
                 ref={scrollViewRef}
                 overScrollMode="never"
@@ -72,50 +59,7 @@ export default function AdoptionDetailPage({
                 scrollEventThrottle={16}
             >
                 <AnimatedImage source={{ uri: data.images[0].src }} style={[styles.imageWrapper, imageHeight, styles.image]} />
-                <View style={styles.profileWrapper}>
-                    <Avatar image={data.user.profile} size={50} />
-                    <View>
-                        <Typo variant="title3">{data.user.nickname}</Typo>
-                        <Typo variant="body3" color="placeholder">{`${data.location.sido} ${data.location.gungu}`}</Typo>
-                    </View>
-                </View>
-                <View style={styles.contentWrapper}>
-                    <Typo variant="title2">{data.title}</Typo>
-                    <Typo variant="body4" color="placeholder">
-                        {calculateTimeAgo(data.createdAt)}
-                    </Typo>
-                    <View style={styles.boxWrapper}>
-                        <View style={[styles.paddingVertical, styles.borderBottom, styles.boxContainer]}>
-                            <View style={styles.row}>
-                                <View style={styles.boxWidth}>
-                                    <Typo color="sub-placeholder">종</Typo>
-                                </View>
-                                <Typo>{`${data.variety.classification} / ${data.variety.species}`}</Typo>
-                            </View>
-                            <View style={styles.row}>
-                                <View style={styles.boxWidth}>
-                                    <Typo color="sub-placeholder">모프</Typo>
-                                </View>
-                                <Typo>{`${data.variety.detailedSpecies} / ${data.variety.morph?.join(' • ')}`}</Typo>
-                            </View>
-                        </View>
-                        <View style={[styles.row, styles.gap, styles.paddingVertical]}>
-                            <View style={styles.row}>
-                                <View style={styles.boxWidth}>
-                                    <Typo color="sub-placeholder">성별</Typo>
-                                </View>
-                                <Typo>{`${formatToGender(data.gender)}`}</Typo>
-                            </View>
-                            <View style={styles.row}>
-                                <View style={styles.boxWidth}>
-                                    <Typo color="sub-placeholder">크기</Typo>
-                                </View>
-                                <Typo>{`${data.size}`}</Typo>
-                            </View>
-                        </View>
-                    </View>
-                    <Typo>{data.content}</Typo>
-                </View>
+                <AdoptionDetailContent data={data} />
             </Animated.ScrollView>
             <View style={styles.buttonWrapper}>
                 <LikeIcon isLike />
@@ -134,15 +78,6 @@ const styles = StyleSheet.create({
     wrapper: {
         position: 'relative',
         paddingBottom: 20,
-    },
-    headerWrapper: {
-        position: 'absolute',
-        flexDirection: 'row',
-        width: '100%',
-        paddingHorizontal: 20,
-        height: HEADER_HEIGHT,
-        justifyContent: 'space-between',
-        zIndex: 20,
     },
     scrollViewContent: {
         flexGrow: 1,
@@ -166,48 +101,6 @@ const styles = StyleSheet.create({
     image: {
         width: '100%',
         aspectRatio: 1,
-    },
-    profileWrapper: {
-        flexDirection: 'row',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        gap: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: color.Gray[200].toString(),
-        backgroundColor: color.White.toString(),
-        zIndex: 10,
-    },
-    contentWrapper: {
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        backgroundColor: color.White.toString(),
-        zIndex: 10,
-    },
-    boxWrapper: {
-        backgroundColor: color.Gray[200].toString(),
-        borderRadius: 10,
-        paddingHorizontal: 10,
-        gap: 5,
-        marginVertical: 20,
-    },
-    boxContainer: {
-        gap: 5,
-    },
-    borderBottom: {
-        borderBottomWidth: 1,
-        borderBottomColor: color.Gray[500].toString(),
-    },
-    row: {
-        flexDirection: 'row',
-    },
-    gap: {
-        gap: 20,
-    },
-    paddingVertical: {
-        paddingVertical: 10,
-    },
-    boxWidth: {
-        width: 40,
     },
     buttonWrapper: {
         flexDirection: 'row',
